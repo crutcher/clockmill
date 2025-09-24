b84ad897307050c01c8781ead16f0104763c934a
Crutcher Dunnavant <crutcher@gmail.com>
2025-09-23 17:09:28 -0700

    Add CUDA support and implement Conway's Game of Life example
    - Updated `burn` dependency with CUDA features in `Cargo.toml`.
    - Added CUDA-based implementation of Conway's Game of Life in `main.rs`.
    - Enhanced argparse options for grid size and steps.

--- examples/scratch/src/main.rs
+++ examples/scratch/src/main.rs
@@ -1,3 +1,10 @@
+use std::time::Instant;
+use burn::backend::Cuda;
+use burn::prelude::{s, Backend, Bool, Tensor};
+use burn::tensor::Distribution;
+use burn::tensor::DType::F16;
+use burn::tensor::module::unfold4d;
+use burn::tensor::ops::UnfoldOptions;
 use clap::Parser;
 
 #[derive(Parser, Debug)]
@@ -6,10 +13,64 @@ pub struct Args {
     /// The number of steps to run.
     #[arg(long, default_value = "100")]
     pub steps: usize,
+
+    /// The width and height of the grid.
+    #[arg(long, default_value = "1000")]
+    pub grid_size: usize,
 }
 
 fn main() {
     let args = Args::parse();
 
-    println!("Args: {:#?}", args);
+    run::<Cuda>(&args);
+}
+
+fn run<B: Backend>(args: &Args) {
+    let device = Default::default();
+
+    let n = args.grid_size;
+    let k = args.steps;
+    let warmup = k/10;
+
+    let mut state: Tensor::<B, 2, Bool> = Tensor::<B, 2>::random([n, n], Distribution::Default, &device).greater_elem(0.5);
+
+    let mut t0: Instant = Instant::now();
+    for step in 0..k {
+        if step == warmup {
+            t0 = Instant::now();
+        }
+        state = conway(state);
+    }
+    let t1: Instant = Instant::now();
+
+    let step_rate = (k - warmup) as f64 / (t1 - t0).as_secs_f64();
+    println!("{:.2} steps/sec", step_rate);
+}
+
+fn conway<B: Backend>(state: Tensor<B, 2, Bool>) -> Tensor<B, 2, Bool> {
+    let [h, w] = state.shape().dims();
+
+    let blocks = unfold4d(
+        state.clone().float().cast(F16).reshape([1, 1, h, w]),
+        [3, 3],
+        UnfoldOptions {
+            stride: [1, 1],
+            padding: [0, 0],
+            dilation: [1, 1],
+        }
+    ).int();
+
+    let block_sum = blocks.clone().sum_dim(1);
+    let neighbor_count = block_sum - blocks.slice(s![0, 5, ..]);
+
+    let neighbor_count = neighbor_count.reshape([h-2, w-2]);
+
+    let inner = state.clone().slice(s![1..-1, 1..-1]);
+
+    let survivors = inner.bool_and(neighbor_count.clone().equal_elem(2));
+    let spawns = neighbor_count.equal_elem(3);
+
+    let update = survivors.bool_or(spawns);
+
+    state.slice_assign(s![1..-1, 1..-1], update)
 }
